cbec4c0bb399c40b2f94e5e8994b771c15b6707b
rene <renegmed@yahoo.com>
2018-12-23 11:39:56 -0500

    added precoded Navbar

--- src/chap03/Chapter3.js
+++ src/chap03/Chapter3.js
@@ -4,14 +4,16 @@ import { Link } from 'react-router-dom';
 function Chapter3(props) { 
     return (
       <div className="container-fluid">
-        <div><b>Chap 3 - Graphic Design and Responsive Layout</b></div>   <br/>  
-        <Link to="/BackgroundColors"> Background Colors </Link>  <br/>  
-        <Link to="/ContainersRows"> Containers and Rows </Link>  <br/>  
-        <Link to="/MediaBreakpoints"> Media Breakpoints </Link>  <br/>  
-        <Link to="/GridSystems"> The Grid Systems </Link>  <br/>  
-        <Link to="/AlignmentOffsets"> Alignment and Offsets </Link>  <br/>  
-        <Link to="/Padding"> Padding, Display and Margins </Link>  <br/>  
-        <Link to="/FlexibleBox"> Flexible Box </Link>  <br/>  
+        <div><b>Chap 3 - Graphic Design and Responsive Layout</b></div>
+        <section className="d-flex flex-column"> 
+          <Link to="/BackgroundColors"> Background Colors </Link>  
+          <Link to="/ContainersRows"> Containers and Rows </Link>   
+          <Link to="/MediaBreakpoints"> Media Breakpoints </Link> 
+          <Link to="/GridSystems"> The Grid Systems </Link> 
+          <Link to="/AlignmentOffsets"> Alignment and Offsets </Link> 
+          <Link to="/Padding"> Padding, Display and Margins </Link> 
+          <Link to="/FlexibleBox"> Flexible Box </Link> 
+        </section>    
       </div>
     );
    
--- src/chap04/Chapter4.js
+++ src/chap04/Chapter4.js
@@ -4,17 +4,19 @@ import { Link } from 'react-router-dom';
 function Chapter4(props) { 
     return (
       <div className="container-fluid">
-        <div><b>Chap 4 - Bootstrap Navigation</b></div>  
-        <Link to="/PrecodedNavbar"> Adding the Precoded Navbar </Link>   
-        <Link to="/HandcodedNavbar"> Hand Coding the Navbar </Link>  
-        <Link to="/ARIADataDefinitions"> Navar A.R.I.A and Data-Definitions </Link>   
-        <Link to="/NavbarDropdown"> Navbar with a Dropdown </Link> 
-        <Link to="/OtherNav"> Other Navigations </Link> 
-        <Link to="/NavbarColors"> Navbar Colors </Link>
-        <Link to="/NavbarSearchBox"> Navbar with Search Box </Link> 
-        <Link to="/Breadcrumbs"> Breadcrumbs </Link> 
-        <Link to="/Pagination"> Pagination </Link> 
-        <Link to="/AnimatedScroll"> AnimatedScroll </Link>  
+        <div><b>Chap 4 - Bootstrap Navigation</b></div>
+        <section className="d-flex flex-column"> 
+          <Link to="/PrecodedNavbar"> Adding the Precoded Navbar </Link>   
+          <Link to="/HandcodedNavbar"> Hand Coding the Navbar </Link>  
+          <Link to="/ARIADataDefinitions"> Navar A.R.I.A and Data-Definitions </Link>   
+          <Link to="/NavbarDropdown"> Navbar with a Dropdown </Link> 
+          <Link to="/OtherNav"> Other Navigations </Link> 
+          <Link to="/NavbarColors"> Navbar Colors </Link>
+          <Link to="/NavbarSearchBox"> Navbar with Search Box </Link> 
+          <Link to="/Breadcrumbs"> Breadcrumbs </Link> 
+          <Link to="/Pagination"> Pagination </Link> 
+          <Link to="/AnimatedScroll"> AnimatedScroll </Link>
+        </section>  
       </div>
     );
    
