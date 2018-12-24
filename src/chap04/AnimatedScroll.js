fd14e6840b6583e545bd1929598e31ec42b4f6aa
rene <renegmed@yahoo.com>
2018-12-23 19:02:01 -0500

    chap4 - animated scroll (not working)

--- src/chap04/AnimatedScroll.js
+++ src/chap04/AnimatedScroll.js
@@ -2,8 +2,45 @@ import React from 'react';
 
 function AnimatedScroll(props) {  
     return (
-      <div> 
-        
+      <div>  
+        <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div className="container">
+              <a className="navbar-brand js-scroll-trigger" href="#top">Navbar</a>
+              <button className="navbar-toggler navbar-toggler-right" 
+                  type="button" 
+                  data-toggle="collapse" 
+                  data-target="#navbarResponsive"
+                  aria-controls="navbarResponsive"
+                  aria-expanded="false"
+                  aria-label="Toggle Navigation">Menu <i className="fa fa-bars"></i></button>
+              <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav text-uppercase ml-auto">
+                  <li className="navbar-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="/"
+                        id="navDropdown" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                    <div className="dropdown-menu" aria-labelledby="navDropdown">
+                      <a className="dropdown-item" href="/" >Sub-Link 1</a>
+                      <a className="dropdown-item" href="/" >Sub-Link 2</a>
+                      <div className="dropdown-divider"></div>
+                      <a className="dropdown-item" href="/" >Sub-Link 3</a>
+                    </div>
+                  </li>  
+                  <li className="nav-item active"><a className="nav-link js-scroll-trigger" href="#link1">Link1</a></li>
+                  <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#link2">Link2</a></li>
+                  <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#link3">Link3</a></li>
+                  <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#link4">Link4</a></li>
+                  <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#link5">Link5</a></li>
+                </ul>
+              </div>
+            </div>
+        </nav>
+        <header className="masthead2"> 
+          <div className="container">
+            <div className="intro-text"></div>
+          </div>
+        </header>
+        <div className="heightAdder"></div>
       </div>
     );
    
--- src/chap04/Pagination.js
+++ src/chap04/Pagination.js
@@ -2,9 +2,199 @@ import React from 'react';
 
 function Pagination(props) {  
     return (
-      <div> 
-        
+    <div> 
+      <hr/>
+      <div className="container m-5">
+        <h4>Chapters</h4> 
+        <div>
+          <nav aria-label="Chapter Links">
+            <ul className="pagination">
+              <li className="page-item">
+                <a className="page-link" href="/" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                  <span className="sr-only">Previous</span>
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">1</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">2</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">3</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                  <span className="sr-only">Next</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
+
+      <hr/>
+      <div className="container m-5">
+        <h4>Disable Previous Button</h4> 
+        <div>
+          <nav aria-label="Chapter Links">
+            <ul className="pagination">
+              <li className="page-item disabled">
+                <a className="page-link" href="/" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                  <span className="sr-only">Previous</span>
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">1</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">2</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">3</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                  <span className="sr-only">Next</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <div className="container m-5">
+        <h4>Active Page Button</h4> 
+        <div>
+          <nav aria-label="Chapter Links">
+            <ul className="pagination">
+              <li className="page-item">
+                <a className="page-link" href="/" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                  <span className="sr-only">Previous</span>
+                </a>
+              </li>
+              <li className="page-item active">
+                <a className="page-link" href="/">1</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">2</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">3</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                  <span className="sr-only">Next</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <div className="container m-5">
+        <h4>Pagination large button size</h4> 
+        <div>
+          <nav aria-label="Chapter Links">
+            <ul className="pagination pagination-lg">
+              <li className="page-item disabled">
+                <a className="page-link" href="/" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                  <span className="sr-only">Previous</span>
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">1</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">2</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">3</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                  <span className="sr-only">Next</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+
+      <div className="container m-5">
+        <h4>Center Justify</h4> 
+        <div>
+          <nav aria-label="Chapter Links">
+            <ul className="pagination pagination-sm justify-content-center">
+              <li className="page-item disabled">
+                <a className="page-link" href="/" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                  <span className="sr-only">Previous</span>
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">1</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">2</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">3</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                  <span className="sr-only">Next</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <div className="container m-5">
+        <h4>End Justify</h4> 
+        <div>
+          <nav aria-label="Chapter Links">
+            <ul className="pagination pagination-sm justify-content-end">
+              <li className="page-item disabled">
+                <a className="page-link" href="/" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                  <span className="sr-only">Previous</span>
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">1</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">2</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/">3</a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="/" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                  <span className="sr-only">Next</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+
+    </div>  
+
     );
    
 }
