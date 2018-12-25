696f307b32d5ff0f45b10a478d8059a0ce2a6cef
rene <renegmed@yahoo.com>
2018-12-25 11:22:35 -0500

    chap5 - inline tags

--- src/chap05/InlineTags.js
+++ src/chap05/InlineTags.js
@@ -4,7 +4,20 @@ function InlineTags(props) {
     return (
       <div> 
         <div>Inline Tags</div>
- 
+        <div className="container mt-3">
+          <h1 className="display-4">List of Characters</h1>
+          <ul>
+            <li><mark>Alice</mark></li>
+            <li><mark>Queen of Hearts</mark></li>
+            <li><s>Cheshire Cat</s></li>
+            <li><ins>Tweede Dee</ins></li>
+            <li><u>Tweedle Dum</u></li>
+            <li><small>Mad Hatter</small></li>
+            <li><strong>White Rabbit</strong></li>
+            <li><em>March Hare</em></li>
+            <li>Big <sub>Catepillar</sub></li> 
+          </ul>
+        </div>
       </div>
     );
    
